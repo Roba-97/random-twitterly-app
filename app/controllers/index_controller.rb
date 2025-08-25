9949126ec21f5219060c284aad37d29875c504e8
Roba-97 <ryota@Roba.>
2025-08-25 17:46:50 +0900

    Move IndexController to PostsController

--- app/controllers/index_controller.rb
+++
@@ -1,5 +0,0 @@
-class IndexController < ApplicationController
-  def index
-    @theme = Theme.order("RANDOM()").first
-  end
-end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    @theme = Theme.find(params[:theme_id])
+    @nextTheme = Theme.order("RANDOM()").first
   end
 
   def create
