08cfd0c4e9193351f27836d8942866c95a753c1e
Roba-97 <ryota@Roba.>
2025-08-25 18:01:42 +0900

    Add basic posting system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,18 @@ class PostsController < ApplicationController
   def index
     @theme = Theme.find(params[:theme_id])
     @nextTheme = Theme.order("RANDOM()").first
+    @post = Post.build
   end
 
   def create
+    @post = Theme.find(params[:theme_id]).posts.build(post_params)
+    @post.save
+    flash[:success] = "投稿しました"
+    redirect_to theme_posts_path(params[:theme_id])
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
